6c2bf69e3f76bda53d4c4342196cdbbe43dc1f68
Shyam <shyam.kumar@letsventure.com>
2017-05-26 22:20:58 +0530

    Feat: Project Setup

---
+++ MeMe-1.0/MMHomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MMHomeViewController.swift
+//  MeMe-1.0
+//
+//  Created by Shyam on 26/05/17.
+//  Copyright © 2017 Shyam. All rights reserved.
+//
+
+import UIKit
+
+class MMHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
